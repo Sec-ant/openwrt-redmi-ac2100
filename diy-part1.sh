1945051ddb604944cc7834bccf37df3cd2a80bdc
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-01-29 17:07:48 +0000

    init

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Uncomment feed sources
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Add feed sources
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore' >> feeds.conf.default
+echo 'src-git wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
